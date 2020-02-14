30b7a094b4d02b0c0d864c0d3e6aa85544dc0e98
ljh-c <57099181+ljh-c@users.noreply.github.com>
2020-02-14 08:28:56 -0800

    sets up routers

--- server.js
+++ server.js
@@ -2,10 +2,23 @@ const express = require('express');
 
 const server = express();
 
+const projectsRouter = require('./routers/projectsRouter');
+const actionsRouter = require('./routers/actionsRouter');
+
 server.use(express.json());
+
+// logger middleware
+
+function logger(req, res, next) {
+  console.log(`${req.method} Request to ${req.originalUrl}`);
+  next();
+}
 
 server.get('/', (req, res) => {
   res.send(`<h1>Don't Worry Be Happy</h1>`);
 });
+
+server.use('/api/projects', logger, projectsRouter);
+server.use('/api/actions', logger, actionsRouter);
 
 module.exports = server;
