15838fb6710549a153cf150cce4f509bfbc3d208
ljh-c <57099181+ljh-c@users.noreply.github.com>
2020-02-14 08:35:25 -0800

    adds endpoint: read projects

--- routers/projectsRouter.js
+++ routers/projectsRouter.js
@@ -4,4 +4,14 @@ const router = express.Router();
 
 const Projects = require('../data/helpers/projectModel');
 
+// READ PROJECTS
+router.get('/', async (req, res) => {
+  try {
+    res.status(200).json(await Projects.get());
+  }
+  catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
 module.exports = router;
