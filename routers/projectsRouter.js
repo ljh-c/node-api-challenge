40ec8020ef3e53304e93110625017e36d3be6f1e
ljh-c <57099181+ljh-c@users.noreply.github.com>
2020-02-14 09:48:16 -0800

    adds endpoint: add action

--- routers/projectsRouter.js
+++ routers/projectsRouter.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 const Projects = require('../data/helpers/projectModel');
+const Actions = require('../data/helpers/actionModel');
 
 // READ PROJECTS
 router.get('/', async (req, res) => {
@@ -23,16 +24,6 @@ router.get('/:id', validateProjectId, (req, res) => {
     res.status(500).json({ error: err.message });
   }
 })
-
-// READ ACTIONS FOR A PROJECT
-router.get('/:id/actions', validateProjectId, async (req, res) => {
-  try {
-    res.status(200).json(await Projects.getProjectActions(req.params.id));
-  }
-  catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
 
 // ADD PROJECT
 
@@ -63,6 +54,30 @@ router.put('/:id', validateProjectId, async (req, res) => {
   try {
     const updatedProject = await Projects.update(req.project.id, req.body);
     res.status(200).json(updatedProject);
+  }
+  catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// READ ACTIONS FOR A PROJECT
+router.get('/:id/actions', validateProjectId, async (req, res) => {
+  try {
+    res.status(200).json(await Projects.getProjectActions(req.params.id));
+  }
+  catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// ADD ACTION FOR A PROJECT
+
+router.post('/:id/actions', validateProjectId, async (req, res) => {
+  try {
+    req.body.project_id = req.project.id;
+
+    const newAction = await Actions.insert(req.body);
+    res.status(201).json(newAction);
   }
   catch (err) {
     res.status(500).json({ error: err.message });
